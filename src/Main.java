a491e893f618cae8b67f5406e53fee66be3c194d
Yana Kalugina <yanakaluginayana@gmail.com>
2022-06-23 21:32:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        int costPerMile = 20;
+
+        int miles = amount / costPerMile;
+
+        System.out.println(miles);
+    }
+}
